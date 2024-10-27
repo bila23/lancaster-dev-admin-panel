e56b8b5b17ae3e3f955e1e95a018b9b18fccba64
William Juarez <dev@lancaster.com.pe>
2024-10-27 11:24:58 -0600

    finish save update

--- src/components/deliverfree/deliverFreeQuery.jsx
+++ src/components/deliverfree/deliverFreeQuery.jsx
@@ -1,7 +1,129 @@
 import React, { useState, useEffect, useRef } from "react";
+import service from "../../service/modules/deliverFreeService";
+import util from "../../service/common/util";
+import Modal from "../common/modal";
+import DeliverFreeSave from "./deliverFreeSave";
+import Btn from "../common/btn";
+import { Card } from "primereact/card";
+import { Toast } from "primereact/toast";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 const DeliverFreeQuery = () => {
-  return <></>;
+  const toast = useRef(null);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [saveDlg, setSaveDlg] = useState(false);
+  const [model, setModel] = useState("");
+  const [editDlg, setEditDlg] = useState(false);
+
+  const find = async () => {
+    setLoading(true);
+    try {
+      const result = await service.findAll();
+      setData(result);
+    } catch (e) {
+      console.error(e);
+      util.error(toast, "Ha ocurrido un error al recuperar los datos");
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    find();
+  }, []);
+
+  const prepareEdit = (rowData) => {
+    setModel(rowData);
+    setEditDlg(true);
+  };
+
+  const actionBody = (rowData) => {
+    return (
+      <button
+        className="btn-floating btn-medium waves-effect waves-light darken-3 green"
+        title="Editar"
+        onClick={(e) => prepareEdit(rowData)}
+      >
+        <i className="material-icons">edit</i>
+      </button>
+    );
+  };
+
+  return (
+    <>
+      <Toast ref={toast} />
+      <Modal flag={saveDlg} setFlag={setSaveDlg} header="Nuevo Registro">
+        <DeliverFreeSave mode="SAVE" toast={toast} find={find} />
+      </Modal>
+      <Modal flag={editDlg} setFlag={setEditDlg} header="Editar Registro">
+        {model && (
+          <DeliverFreeSave
+            mode="UPDATE"
+            toast={toast}
+            find={find}
+            model={model}
+          />
+        )}
+      </Modal>
+      <div className="row">
+        <div className="col s12 m6">
+          <h4 className="strong">Deliver Gratis</h4>
+        </div>
+        <div className="col s12 m6 right-align">
+          <h4>
+            <Btn icon="refresh" name="Actualizar" execute={find} />
+            <Btn
+              icon="add"
+              name="Nuevo Registro"
+              execute={() => setSaveDlg(true)}
+              style={{ marginLeft: "10px" }}
+            />
+          </h4>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          <Card>
+            <div className="row">
+              <div className="col s12">
+                <DataTable
+                  value={data}
+                  loading={loading}
+                  stripedRows
+                  emptyMessage="No hay registro(s)"
+                >
+                  <Column
+                    field="fechaCreaString"
+                    header="Fecha Creaci&oacute;n"
+                    sortable
+                    filter
+                    filterMatchMode="contains"
+                  />
+                  <Column
+                    field="valor"
+                    header="Valor"
+                    sortable
+                    filter
+                    filterMatchMode="contains"
+                  />
+                  <Column
+                    field="activo"
+                    header="Activo"
+                    sortable
+                    filter
+                    body={util.activoBody}
+                    filterMatchMode="contains"
+                  />
+                  <Column body={actionBody} />
+                </DataTable>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DeliverFreeQuery;
---
+++ src/components/deliverfree/deliverFreeSave.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import service from "../../service/modules/deliverFreeService";
+import util from "../../service/common/util";
+import Btn from "../common/btn";
+import { InputNumber } from "primereact/inputnumber";
+
+const DeliverFreeSave = ({ find, toast, mode, model }) => {
+  const [valor, setValor] = useState("");
+
+  const init = () => {
+    if (mode === "UPDATE") fillForm();
+  };
+
+  const fillForm = () => {
+    setValor(model.valor);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const validateForm = () => {
+    if (!valor) {
+      util.warning(toast, "Debe ingresar el valor");
+      return false;
+    } else return true;
+  };
+
+  const doSave = async () => {
+    if (validateForm()) {
+      const body = { valor };
+      const { type, message } = await service.save(body);
+      if (type !== "SUCCESS") util.warning(toast, message);
+      else {
+        util.success(toast, "Se ha guardado correctamente el registro");
+        clearForm();
+        await find();
+      }
+    }
+  };
+
+  const doUpdate = async () => {
+    if (validateForm()) {
+      const body = { valor };
+      const { type } = await service.update(model._id, body);
+      if (type !== "SUCCESS")
+        util.error(toast, "No se ha podido actualizar el registro");
+      else {
+        util.success(toast, "Se ha actualizado correctamente el registro");
+        await find();
+      }
+    }
+  };
+
+  const clearForm = () => {
+    setValor("");
+  };
+
+  return (
+    <>
+      <div className="row">
+        <div className="col s12 input-field">
+          <div>Ingrese el valor del Delivery:</div>
+          <InputNumber
+            value={valor}
+            mode="decimal"
+            maxFractionDigits={2}
+            onValueChange={(e) => setValor(e.value)}
+            style={{ width: "100%" }}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          {mode === "SAVE" && (
+            <Btn
+              name="Guardar"
+              icon="add"
+              execute={doSave}
+              style={{ width: "100%" }}
+            />
+          )}
+          {mode === "UPDATE" && (
+            <Btn
+              name="Actualizar"
+              icon="edit"
+              execute={doUpdate}
+              style={{ width: "100%" }}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DeliverFreeSave;
