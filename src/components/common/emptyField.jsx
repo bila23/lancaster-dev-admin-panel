49e9c796571ea91afb4b81af0f8f52b5a8c509e0
William Juarez <dev@lancaster.com.pe>
2024-12-27 15:38:41 -0600

    common

--- src/components/common/emptyField.jsx
+++ src/components/common/emptyField.jsx
@@ -7,7 +7,7 @@ const EmptyField = (props) => {
         className="p-d-flex p-ai-center p-dir-col center-align"
         style={{ marginTop: "20px" }}
       >
-        <img src="/images/theme/empty_1.png" alt="Empty" title="" />
+        <img src="/images/common/empty_1.png" alt="Empty" title="" />
         <h5
           style={{
             color: "var(--text-color-secondary)",
