f60a55c43b87425e9dc8bfc17c857cf67029dd0b
William Juarez <dev@lancaster.com.pe>
2024-12-27 15:14:56 -0600

    DmfSave

---
+++ src/components/delMontoFree/dmfSave.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import service from "../../service/modules/deliveryMontoFreeService";
+import SaveUpdateForm from "../common/saveUpdateForm";
+
+const DmfSave = ({ tipo, id, find, model, isUpdate }) => {
+  const initialForm = {
+    monto: "",
+  };
+  const requiredFields = ["monto"];
+  const arrayInputs = [
+    {
+      id: "monto",
+      type: "number",
+      label: "Ingrese el monto:",
+    },
+  ];
+
+  return (
+    <>
+      <SaveUpdateForm
+        inputs={arrayInputs}
+        initialForm={initialForm}
+        requiredFields={requiredFields}
+        service={service}
+        find={find}
+        isUpdate={isUpdate}
+        model={model}
+      />
+    </>
+  );
+};
+
+export default DmfSave;
