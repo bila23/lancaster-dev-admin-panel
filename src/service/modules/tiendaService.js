b9d90255258e7bd11b6be5c5ccc353b09e075b69
William Juarez <dev@lancaster.com.pe>
2024-12-27 16:08:20 -0600

    service new tiendas

---
+++ src/service/modules/tiendaService.js
@@ -0,0 +1,36 @@
+import http from "../common/httpService";
+
+const api = "/tiendas";
+
+async function findAll() {
+  const { data } = await http.get(api);
+  return data;
+}
+
+async function save(body) {
+  const { data } = await http.post(api, body);
+  return data;
+}
+
+async function update(id, body) {
+  const { data } = await http.put(`${api}/${id}`, body);
+  return data;
+}
+
+async function findById(id) {
+  const { data } = await http.get(`${api}/${id}`);
+  return data;
+}
+
+async function deleteById(id) {
+  const { data } = await http.delete(`${api}/${id}`);
+  return data;
+}
+
+export default {
+  update,
+  findById,
+  findAll,
+  deleteById,
+  save,
+};
