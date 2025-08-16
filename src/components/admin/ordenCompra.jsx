198ded7831bb04ed9ab55a427e92b37bc4ffa27f
William Juarez <wgbila@gmail.com>
2025-08-16 17:32:52 -0600

    se quito react

--- src/components/admin/ordenCompra.jsx
+++ src/components/admin/ordenCompra.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import ordenCompraService from "../../service/modules/ordenCompraService";
 import util from "../../service/common/util";
 import Moment from "react-moment";
